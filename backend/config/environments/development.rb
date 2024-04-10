b8303879e1391f3156c7189392f92b7ae984e1f2
the-bears-field <thebearsfield+github@gmail.com>
2024-04-10 21:26:18 +0900

    feat: バックエンド側へのパスを変更
    nginx.conf
    serverモジュールをフロントエンド側とバックエンド側とを分割。
    バックエンド側のserver_nameを定義し、locationのパスをルートに変更。
    
    development.rb
    config.relative_url_rootを定義する行は不要のため削除。

--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -68,6 +68,4 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
-
-  config.relative_url_root = "/api"
 end
