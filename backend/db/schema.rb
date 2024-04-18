f675d3c6080655e4edf82fbc336f36a575ac9b8a
the-bears-field <thebearsfield+github@gmail.com>
2024-04-18 14:50:41 +0900

    feat(backend): Commentモデル作成、マイグレーションを実行
    参考:
    https://railsguides.jp/active_record_migrations.html
    https://railsdoc.com/page/create_table

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_18_054529) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_18_054755) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "description", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title", null: false
