85c7ec6e3e9a1f3631e7446a4a6fdf1bf0f7db63
the-bears-field <thebearsfield+github@gmail.com>
2024-04-18 14:54:32 +0900

    feat(backend): UserPostモデル作成、マイグレーションを実行
    参考:
    https://railsguides.jp/active_record_migrations.html
    https://railsdoc.com/page/create_table

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_18_054755) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_18_055135) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,6 +33,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_18_054755) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "user_posts", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_user_posts_on_post_id"
+    t.index ["user_id"], name: "index_user_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
@@ -41,4 +50,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_18_054755) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "user_posts", "posts"
+  add_foreign_key "user_posts", "users"
 end
