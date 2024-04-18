dae6bb2b77d0e552c7b199e6442e553cd771ba7b
the-bears-field <thebearsfield+github@gmail.com>
2024-04-18 14:37:21 +0900

    feat(backend): Userモデル作成、マイグレーションを実行
    コンテナ内で以下のコマンドを実行しファイルを生成
    bin/rails g model User name:string email:string password:string
    
    参考:
    https://railsguides.jp/active_record_migrations.html
    https://railsdoc.com/page/create_table

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,8 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_09_053658) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_18_051431) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
