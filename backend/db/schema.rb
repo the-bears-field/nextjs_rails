52c37a01b2fd3c69f71c5fc08aaf8240340ccc12
the-bears-field <thebearsfield+github@gmail.com>
2024-04-18 14:46:47 +0900

    feat(backend): Tagモデル作成、マイグレーションを実行
    参考:
    https://railsguides.jp/active_record_migrations.html
    https://railsdoc.com/page/create_table

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_18_053833) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_18_054529) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
     t.string "title", null: false
     t.text "description", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
