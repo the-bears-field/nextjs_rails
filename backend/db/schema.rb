68f4cafd1fd76a08ecf2588ee8ea8210272eff83
the-bears-field <thebearsfield+github@gmail.com>
2024-04-18 19:41:10 +0900

    feat(backend): PostCommentモデル作成、マイグレーションを実行
    参考:
    https://railsguides.jp/active_record_migrations.html
    https://railsdoc.com/page/create_table

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_18_055530) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_18_104001) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_18_055530) do
     t.text "description", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "post_comments", force: :cascade do |t|
+    t.bigint "post_id", null: false
+    t.bigint "comment_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["comment_id"], name: "index_post_comments_on_comment_id"
+    t.index ["post_id"], name: "index_post_comments_on_post_id"
   end
 
   create_table "post_tags", force: :cascade do |t|
@@ -59,6 +68,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_18_055530) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "post_comments", "comments"
+  add_foreign_key "post_comments", "posts"
   add_foreign_key "post_tags", "posts"
   add_foreign_key "post_tags", "tags"
   add_foreign_key "user_posts", "posts"
