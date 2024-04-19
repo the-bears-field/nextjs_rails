4169ce46610b090903f2c145a38b48fb08f91fbc
the-bears-field <thebearsfield+github@gmail.com>
2024-04-19 10:44:02 +0900

    feat(backend): Userコントローラ作成
    参考:
    https://railsguides.jp/v7.1/getting_started.html

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  post 'users/create'
+  get 'users/edit'
+  post 'users/update'
+  get 'users/destory'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
