d213ba4e2b892bc1ff18b42acb72994690d46c58
the-bears-field <thebearsfield+github@gmail.com>
2024-04-26 14:51:22 +0900

    feat(backend): Userモデルに対象の属性値が重複していないことを検証するバリデーションを追加
    参考:
    https://railsguides.jp/active_record_validations.html#uniqueness

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   has_many :user_post
   has_many :user_comment
   validates :name, :email, :password_digest, presence: true
+  validates :email, uniqueness: true
 end
