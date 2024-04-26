412cb0f8ef0fda2121cc5ad00aa11396ccd748e1
the-bears-field <thebearsfield+github@gmail.com>
2024-04-26 20:04:03 +0900

    test(backend): Userモデルスペックにテストを追加
    参考:
    https://railsguides.jp/active_record_validations.html#valid-questionmark%E3%81%A8invalid-questionmark
    https://rspec.info/documentation/6.1/rspec-rails/
    Everyday Rails - RSpecによるRailsテスト入門 p.33-45

--- backend/spec/models/user_spec.rb
+++ backend/spec/models/user_spec.rb
@@ -1,5 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # ユーザー名、メール、パスワードが有効な状態であること
+  it "is valid with a user name, email, and password" do
+    user = User.new(
+      name: 'John Doe',
+      email: 'test@example.com',
+      password_digest: 'password'
+    )
+    expect(user).to be_valid
+  end
+
+  # ユーザー名がなければ無効な状態であること
+  it "is invalid without a user name" do
+    user = User.new(
+      name: nil,
+      email: 'test@example.com',
+      password_digest: 'password'
+    )
+    user.valid?
+    expect(user.errors[:name]).to include("can't be blank")
+  end
+
+  # メールアドレスがなければ無効な状態であること
+  it "is invalid without an email address" do
+    user = User.new(
+      name: 'John Doe',
+      email: nil,
+      password_digest: 'password'
+    )
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
+  end
+
+  # 重複したメールアドレスなら無効な状態であること
+  it "is invalid with a duplicate email address" do
+    User.create(
+      name: 'John Doe',
+      email: 'test@example.com',
+      password_digest: 'password'
+    )
+
+    user = User.new(
+      name: 'John Doe',
+      email: 'test@example.com',
+      password_digest: 'password'
+    )
+
+    user.valid?
+    expect(user.errors[:email]).to include("has already been taken")
+  end
 end
