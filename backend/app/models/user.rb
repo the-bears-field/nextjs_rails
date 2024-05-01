86615413cbae6bea2b844c7b09664828ec499786
the-bears-field <thebearsfield+github@gmail.com>
2024-05-01 21:48:03 +0900

    feat(backend): 各モデルにクエリを実行するための関連付けを指定する記述を追加
    参考:
    https://railsguides.jp/association_basics.html#has-many-through%E9%96%A2%E9%80%A3%E4%BB%98%E3%81%91
    https://api.rubyonrails.org/v7.1/classes/ActiveRecord/Associations/ClassMethods.html#module-ActiveRecord::Associations::ClassMethods-label-Many-to-many
    https://api.rubyonrails.org/v7.1/classes/ActiveRecord/Associations/ClassMethods.html#method-i-has_many

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :user_post
+  has_many :post, through: :user_post
   has_many :user_comment
+  has_many :comment, through: :user_comment
   validates :name, :email, :password_digest, presence: true
   validates :email, uniqueness: true
 end
