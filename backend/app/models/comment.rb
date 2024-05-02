eeab3364636abe75e83982ebe1bd6c92b183f2b1
the-bears-field <thebearsfield+github@gmail.com>
2024-05-02 14:52:38 +0900

    fix(backend): Commentモデルのhas_many関連付けの一部に誤りがあったため修正

--- backend/app/models/comment.rb
+++ backend/app/models/comment.rb
@@ -2,6 +2,6 @@ class Comment < ApplicationRecord
   has_many :post_comment
   has_many :post, through: :post_comment
   has_many :user_comment
-  has_many :comment, through: :user_comment
+  has_many :user, through: :user_comment
   validates :description, presence: true
 end
