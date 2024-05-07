afd4c2f2889cb956120d77765c04539ced2ad9aa
the-bears-field <thebearsfield+github@gmail.com>
2024-05-07 14:54:13 +0900

    feat(backend): Factory Botでの運用に向けてRailsの設定ファイルを編集
    ジェネレータを使って作られるモデルに対して、⾃動的にファクトリを作成するよう、不要な箇所を削除。
    
    参考:
    Everyday Rails - RSpecによるRailsテスト入門 p.61

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -31,11 +31,9 @@ module App
 
     config.generators do |g|
       g.test_framework :rspec,
-        fixtures: false,
         view_specs: false,
         helper_specs: false,
         routing_specs: false
-      g.factory_bot false
     end
   end
 end
