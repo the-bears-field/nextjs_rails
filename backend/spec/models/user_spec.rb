549007e039d513f1f14ab0b8960ec70b20067dcb
the-bears-field <thebearsfield+github@gmail.com>
2024-05-18 19:17:08 +0900

    test(backend): itに渡す文字列を日本語に変更

--- backend/spec/models/user_spec.rb
+++ backend/spec/models/user_spec.rb
@@ -3,35 +3,30 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   let(:user) { FactoryBot.create(:user) }
 
-  # ユーザー名、メール、パスワードが有効なファクトリを持つこと
-  it "has a valid factory" do
+  it "ユーザー名、メール、パスワードが有効なファクトリを持つこと" do
     expect(user).to be_valid
   end
 
-  # ユーザー名がなければ無効な状態であること
-  it "is invalid without a user name" do
+  it "ユーザー名がなければ無効な状態であること" do
     user.name = nil
     user.valid?
     expect(user.errors[:name]).to include("can't be blank")
   end
 
-  # メールアドレスがなければ無効な状態であること
-  it "is invalid without an email address" do
+  it "メールアドレスがなければ無効な状態であること" do
     user.email = nil
     user.valid?
     expect(user.errors[:email]).to include("can't be blank")
   end
 
-  # 重複したメールアドレスなら無効な状態であること
-  it "is invalid with a duplicate email address" do
+  it "重複したメールアドレスなら無効な状態であること" do
     FactoryBot.create(:user, email: 'aaa@example.com')
     user = FactoryBot.build(:user, email: 'aaa@example.com')
     user.valid?
     expect(user.errors[:email]).to include("has already been taken")
   end
 
-  # 複数のユーザーで何かする
-  it "does something with multiple users" do
+  it "複数のユーザーで何かする" do
     user1 = FactoryBot.create(:user)
     user2 = FactoryBot.create(:user)
     expect(true).to eq(true)
