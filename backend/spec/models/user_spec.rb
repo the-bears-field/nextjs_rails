1f2962151d01acb775862c1ddca9cb683d5f055a
the-bears-field <thebearsfield+github@gmail.com>
2024-05-18 19:11:08 +0900

    test(backend): Userモデルファクトリ作成方法をletメソッドを用いた方法に変更。
    参考:
    https://qiita.com/jnchito/items/cdd9eef2ed193267c651
    https://qiita.com/jnchito/items/42193d066bd61c740612#%E3%82%A4%E3%83%B3%E3%82%B9%E3%82%BF%E3%83%B3%E3%82%B9%E5%A4%89%E6%95%B0%E3%81%AE%E3%81%8B%E3%82%8F%E3%82%8A%E3%81%AB-let-%E3%82%92%E4%BD%BF%E3%81%86
    Everyday Rails - RSpecによるRailsテスト入門 138-139
    https://rspec.info/documentation/3.13/rspec-core/RSpec/Core/MemoizedHelpers/ClassMethods.html#let-instance_method

--- backend/spec/models/user_spec.rb
+++ backend/spec/models/user_spec.rb
@@ -1,21 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  let(:user) { FactoryBot.create(:user) }
+
   # ユーザー名、メール、パスワードが有効なファクトリを持つこと
   it "has a valid factory" do
-    expect(FactoryBot.build(:user)).to be_valid
+    expect(user).to be_valid
   end
 
   # ユーザー名がなければ無効な状態であること
   it "is invalid without a user name" do
-    user = FactoryBot.build(:user, name: nil)
+    user.name = nil
     user.valid?
     expect(user.errors[:name]).to include("can't be blank")
   end
 
   # メールアドレスがなければ無効な状態であること
   it "is invalid without an email address" do
-    user = FactoryBot.build(:user, email: nil)
+    user.email = nil
     user.valid?
     expect(user.errors[:email]).to include("can't be blank")
   end
