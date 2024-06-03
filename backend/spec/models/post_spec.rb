aee79456289c9f5581c53b19ee5846e4dc90c0f6
the-bears-field <thebearsfield+github@gmail.com>
2024-06-04 08:17:26 +0900

    test(backend): Postモデルスペックにて投稿の作成方法をFactoryBotを用いた方法に変更

--- backend/spec/models/post_spec.rb
+++ backend/spec/models/post_spec.rb
@@ -1,32 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  # 記事作成が正常に可能であること
-  it "is valid with a title, description" do
-    post = Post.new(
-      title: 'title',
-      description: 'description'
-    )
+  let(:post) { FactoryBot.create(:post) }
+
+  it "記事作成が正常に可能なファクトリであること" do
     expect(post).to be_valid
   end
 
-  # タイトルがなければ無効な状態であること
-  it "is invalid without a title" do
-    post = Post.new(
-      title: nil,
-      description: 'description'
-    )
+  it "タイトルがなければ無効な状態であること" do
+    post.title= nil
     post.valid?
-    expect(post.errors[:title]).to include("can't be blank")
+    expect(post.errors.of_kind?(:title, :blank)).to be_truthy
   end
 
-  # 記事の文章がなければ無効な状態であること
-  it "is invalid without a description" do
-    post = Post.new(
-      title: 'title',
-      description: nil
-    )
+  it "記事の文章がなければ無効な状態であること" do
+    post.description = nil
     post.valid?
-    expect(post.errors[:description]).to include("can't be blank")
+    expect(post.errors.of_kind?(:description, :blank)).to be_truthy
   end
 end
