71b8eba28c617c708b8a95051e70ab30cd8780ca
the-bears-field <thebearsfield+github@gmail.com>
2024-06-03 14:06:44 +0900

    test(backend): ファクトリで異なるバリエーションの作成に対応
    trait内で、関連レコードを生成する関数を実行。
    
    参考:
    https://github.com/thoughtbot/factory_bot/blob/main/GETTING_STARTED.md#traits
    Everyday Rails - RSpecによるRailsテスト入門 p.74

--- backend/spec/factories/users.rb
+++ backend/spec/factories/users.rb
@@ -3,5 +3,28 @@ FactoryBot.define do
     name { Faker::Internet.unique.username }
     email { Faker::Internet.unique.email(domain: 'example.com') }
     password { 'password' }
+
+    # ユーザーと紐づいた投稿を生成
+    trait :with_posts do
+      after(:create) { |user|
+        create_associated_records(
+          main_factory: user,
+          associated_factory_symbol: :post,
+          count: 3
+        )
+      }
+    end
+
+    # ユーザーと紐づいたタグ付済み投稿を生成
+    trait :with_posts_and_tags do
+      after(:create) { |user|
+        create_associated_records(
+          main_factory: user,
+          associated_factory_symbol: :post,
+          associated_factory_traits: [:with_tags],
+          count: 3
+        )
+      }
+    end
   end
 end
