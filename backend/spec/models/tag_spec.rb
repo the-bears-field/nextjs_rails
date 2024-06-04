56a13c981b744d668564e3dccf9a980d364f3387
the-bears-field <thebearsfield+github@gmail.com>
2024-06-05 06:55:21 +0900

    test(backend): Tagモデルスペックのテストオブジェクトの作成方法をファクトリを用いた方法に変更

--- backend/spec/models/tag_spec.rb
+++ backend/spec/models/tag_spec.rb
@@ -1,24 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  # タグ名が有効な状態であること
-  it "is valid with a tag name" do
-    tag = Tag.new(name: 'example')
+  let(:tag) { FactoryBot.build(:tag) }
+
+  it "タグ名が有効な状態であること" do
     expect(tag).to be_valid
   end
 
-  # タグ名がなければ無効な状態であること
-  it "is valid without a tag name" do
-    tag = Tag.new(name: nil)
+  it "タグ名がなければ無効な状態であること" do
+    tag.name = nil
     tag.valid?
-    expect(tag.errors[:name]).to include("can't be blank")
+    expect(tag.errors.of_kind?(:name, :blank)).to be_truthy
   end
 
-  # タグ名が重複しない状態であること
-  it "is invalid with a duplicate tag name" do
-    Tag.create(name: "example")
-    tag = Tag.new(name: "example")
+  it "タグ名が重複しない状態であること" do
+    FactoryBot.create(:tag, name: "example")
+    tag.name = "example"
     tag.valid?
-    expect(tag.errors[:name]).to include("has already been taken")
+    expect(tag.errors.of_kind?(:name, :taken)).to be_truthy
   end
 end
