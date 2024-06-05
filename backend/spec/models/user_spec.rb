0bd85732e98e6f9acb9f7ebe3fc9db22838cb917
the-bears-field <thebearsfield+github@gmail.com>
2024-06-05 13:56:08 +0900

    test(backend): Userモデルスペックの構造を変更
    参考:
    Everyday Rails - RSpecによるRailsテスト入門 p.49-50
    https://qiita.com/jnchito/items/42193d066bd61c740612

--- backend/spec/models/user_spec.rb
+++ backend/spec/models/user_spec.rb
@@ -5,63 +5,70 @@ RSpec.describe User, type: :model do
   let(:user_with_posts) { FactoryBot.create(:user, :with_posts) }
   let(:user_with_posts_and_tags) { FactoryBot.create(:user, :with_posts_and_tags) }
 
-  it "ユーザー名、メール、パスワードが有効なファクトリを持つこと" do
-    expect(user).to be_valid
-  end
+  describe "ユーザーの新規登録" do
+    describe "新規登録が可能である" do
+      it "ユーザー名、メール、パスワードが有効なファクトリを持つこと" do
+        expect(user).to be_valid
+      end
 
-  it "複数のユーザーで何かする" do
-    user1 = FactoryBot.build(:user)
-    user2 = FactoryBot.build(:user)
-    expect(true).to eq(true)
-  end
+      it "複数のユーザーで何かする" do
+        user1 = FactoryBot.build(:user)
+        user2 = FactoryBot.build(:user)
+        expect(true).to eq(true)
+      end
 
-  context "name 名前" do
-    it "nilの場合、無効な状態であること" do
-      user.name = nil
-      user.valid?
-      expect(user.errors.of_kind?(:name, :blank)).to be_truthy
-    end
+      context ':with_posts' do
+        it 'ユーザーと紐づいた投稿が3つ作成されている' do
+          expect(user_with_posts.posts.size).to eq(3)
+        end
+      end
 
-    it "空文字の場合、無効な状態であること" do
-      user.name = ""
-      user.valid?
-      expect(user.errors.of_kind?(:name, :blank)).to be_truthy
-    end
-  end
-
-  context "email メール" do
-    it "nilの場合、無効な状態であること" do
-      user.email = nil
-      user.valid?
-      expect(user.errors.of_kind?(:email, :blank)).to be_truthy
-    end
-
-    it "空文字の場合、無効な状態であること" do
-      user.email = ""
-      user.valid?
-      expect(user.errors.of_kind?(:email, :blank)).to be_truthy
-    end
-
-    it "重複したメールアドレスなら無効な状態であること" do
-      FactoryBot.create(:user, email: 'aaa@example.com')
-      user = FactoryBot.build(:user, email: 'aaa@example.com')
-      user.valid?
-      expect(user.errors.of_kind?(:email, :taken)).to be_truthy
-    end
-  end
-
-  context ':with_posts' do
-    it 'ユーザーと紐づいた投稿が3つ作成されている' do
-      expect(user_with_posts.posts.size).to eq(3)
-    end
-  end
-
-  context ':with_posts_and_tags' do
-    it 'ユーザーと紐づいた投稿が3つ作成、かつ各々の投稿に10個タグ付けされている' do
-      expect(user_with_posts_and_tags.posts.size).to eq(3)
-      user_with_posts_and_tags.posts.each do |post|
-        expect(post.tags.size).to eq(10)
+      context ':with_posts_and_tags' do
+        it 'ユーザーと紐づいた投稿が3つ作成、かつ各々の投稿に10個タグ付けされている' do
+          expect(user_with_posts_and_tags.posts.size).to eq(3)
+          user_with_posts_and_tags.posts.each do |post|
+            expect(post.tags.size).to eq(10)
+          end
+        end
       end
     end
+  end
+
+  describe "新規登録が不可能である" do
+    context "name 名前" do
+      it "nilの場合、無効な状態であること" do
+        user.name = nil
+        user.valid?
+        expect(user.errors.of_kind?(:name, :blank)).to be_truthy
+      end
+
+      it "空文字の場合、無効な状態であること" do
+        user.name = ""
+        user.valid?
+        expect(user.errors.of_kind?(:name, :blank)).to be_truthy
+      end
+    end
+
+    context "email メール" do
+      it "nilの場合、無効な状態であること" do
+        user.email = nil
+        user.valid?
+        expect(user.errors.of_kind?(:email, :blank)).to be_truthy
+      end
+
+      it "空文字の場合、無効な状態であること" do
+        user.email = ""
+        user.valid?
+        expect(user.errors.of_kind?(:email, :blank)).to be_truthy
+      end
+
+      it "重複したメールアドレスなら無効な状態であること" do
+        FactoryBot.create(:user, email: 'aaa@example.com')
+        user = FactoryBot.build(:user, email: 'aaa@example.com')
+        user.valid?
+        expect(user.errors.of_kind?(:email, :taken)).to be_truthy
+      end
+    end
+
   end
 end
