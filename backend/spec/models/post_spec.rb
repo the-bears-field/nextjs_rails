14abc8ec5477176ba43f7ed94ff42bcffe5849b7
the-bears-field <thebearsfield+github@gmail.com>
2024-06-05 14:02:18 +0900

    test(backend): Postモデルスペックの構造を変更
    参考:
    Everyday Rails - RSpecによるRailsテスト入門 p.49-50
    https://qiita.com/jnchito/items/42193d066bd61c740612

--- backend/spec/models/post_spec.rb
+++ backend/spec/models/post_spec.rb
@@ -3,19 +3,42 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   let(:post) { FactoryBot.build(:post) }
 
-  it "記事作成が正常に可能なファクトリであること" do
-    expect(post).to be_valid
+  describe "記事の新規投稿" do
+    describe "新規投稿が可能である" do
+      it "記事作成が正常に可能であること" do
+        expect(post).to be_valid
+      end
+    end
+
+    describe "新規投稿が不可能である" do
+      context "title タイトル" do
+        it "nilの場合、無効な状態であること" do
+          post.title= nil
+          post.valid?
+          expect(post.errors.of_kind?(:title, :blank)).to be_truthy
+        end
+
+        it "空文字の場合、無効な状態であること" do
+          post.title= ""
+          post.valid?
+          expect(post.errors.of_kind?(:title, :blank)).to be_truthy
+        end
+      end
+
+      context "description 文章" do
+        it "nilの場合、無効な状態であること" do
+          post.description = nil
+          post.valid?
+          expect(post.errors.of_kind?(:description, :blank)).to be_truthy
+        end
+
+        it "空文字の場合、無効な状態であること" do
+          post.description = ""
+          post.valid?
+          expect(post.errors.of_kind?(:description, :blank)).to be_truthy
+        end
+      end
+    end
   end
 
-  it "タイトルがなければ無効な状態であること" do
-    post.title= nil
-    post.valid?
-    expect(post.errors.of_kind?(:title, :blank)).to be_truthy
-  end
-
-  it "記事の文章がなければ無効な状態であること" do
-    post.description = nil
-    post.valid?
-    expect(post.errors.of_kind?(:description, :blank)).to be_truthy
-  end
 end
