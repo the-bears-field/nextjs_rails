5f68774d8a7e55a4f3acf8a5266f1584615d358d
the-bears-field <thebearsfield+github@gmail.com>
2024-06-05 14:12:42 +0900

    test(backend): Tagモデルスペックの構造を変更
    一部文章も変更。
    
    参考:
    Everyday Rails - RSpecによるRailsテスト入門 p.49-50
    https://qiita.com/jnchito/items/42193d066bd61c740612

--- backend/spec/models/tag_spec.rb
+++ backend/spec/models/tag_spec.rb
@@ -3,20 +3,36 @@ require 'rails_helper'
 RSpec.describe Tag, type: :model do
   let(:tag) { FactoryBot.build(:tag) }
 
-  it "タグ名が有効な状態であること" do
-    expect(tag).to be_valid
+  describe "タグの新規投稿" do
+    describe "新規投稿が可能である" do
+      it "タグ名が有効な状態であること" do
+        expect(tag).to be_valid
+      end
+    end
+
+    describe "新規投稿が不可能である" do
+      context "name 名前" do
+        it "nilの場合、無効な状態であること" do
+          tag.name = nil
+          tag.valid?
+          expect(tag.errors.of_kind?(:name, :blank)).to be_truthy
+        end
+
+        it "空文字の場合、無効な状態であること" do
+          tag.name = ""
+          tag.valid?
+          expect(tag.errors.of_kind?(:name, :blank)).to be_truthy
+        end
+
+        it "重複しない状態であること" do
+          FactoryBot.create(:tag, name: "example")
+          tag.name = "example"
+          tag.valid?
+          expect(tag.errors.of_kind?(:name, :taken)).to be_truthy
+        end
+      end
+    end
   end
 
-  it "タグ名がなければ無効な状態であること" do
-    tag.name = nil
-    tag.valid?
-    expect(tag.errors.of_kind?(:name, :blank)).to be_truthy
-  end
 
-  it "タグ名が重複しない状態であること" do
-    FactoryBot.create(:tag, name: "example")
-    tag.name = "example"
-    tag.valid?
-    expect(tag.errors.of_kind?(:name, :taken)).to be_truthy
-  end
 end
