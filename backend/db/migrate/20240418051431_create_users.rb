ce12aac670e8bdd173e59893ebf61ade6e35e08f
the-bears-field <thebearsfield+github@gmail.com>
2024-06-10 14:19:13 +0900

    build(backend): usersテーブルとtagテーブルに関わるマイグレーションファイルの変更
    usersテーブルを生成するマイグレーションファイルに、normalized_emailカラムを追加、
    emailカラムとnormalized_emailカラムにインデックスを追加する処理を追記。
    
    tagsテーブルを生成するマイグレーションファイルに、nameカラムにインデックスを追加する処理を追記

--- backend/db/migrate/20240418051431_create_users.rb
+++ backend/db/migrate/20240418051431_create_users.rb
@@ -3,9 +3,13 @@ class CreateUsers < ActiveRecord::Migration[7.1]
     create_table :users do |t|
       t.string :name, null: false
       t.string :email, null: false
+      t.string :normalized_email, null: false
       t.string :password_digest, null: false
 
       t.timestamps
+
+      t.index :email, unique: true
+      t.index :normalized_email, unique: true
     end
   end
 end
