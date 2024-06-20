898408478dc4429b427f1b8034e2d1e065295ad8
the-bears-field <thebearsfield+github@gmail.com>
2024-06-20 14:57:58 +0900

    feat(backend): 各モデルで共通化するメソッドを書き出し
    参考:
    https://docs.ruby-lang.org/ja/latest/class/Object.html#I_SEND
    https://docs.ruby-lang.org/ja/latest/class/Object.html#I_FREEZE
    https://docs.ruby-lang.org/ja/latest/class/Array.html#I_INCLUDE--3F
    https://docs.ruby-lang.org/ja/latest/method/CGI/s/escapeHTML.html
    https://qiita.com/igrep/items/b2fed2d467f8a16f5eb0

--- backend/app/models/application_record.rb
+++ backend/app/models/application_record.rb
@@ -1,3 +1,27 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+
+  # Object.sendメソッド使用にあたり、モデルの属性名を格納したホワイトリスト
+  VALID_ATTRIBUTES = [:name, :email, :title, :description].freeze
+
+  private
+
+  # モデルの文字列の属性に対し、トリミングとエスケープを行う処理
+  #
+  # @param attribute_name [Symbol] トリミングとエスケープを行う属性の名前
+  # @return [String] トリミングとエスケープ後の属性値(文字列)
+  def sanitize_string_attribute(attribute_name)
+    return unless VALID_ATTRIBUTES.include?(attribute_name)
+    value = self.send(attribute_name)
+    return if value.blank?
+    escape_string(value.strip)
+  end
+
+  # 引数の文字列をエスケープする処理
+  #
+  # @param string [String] エスケープ前の文字列
+  # @return [String] エスケープ後の文字列
+  def escape_string(string)
+    CGI.escapeHTML(string) if string.present?
+  end
 end
