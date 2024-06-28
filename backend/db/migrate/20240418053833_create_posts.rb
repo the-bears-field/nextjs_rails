3355f7a242bdb826976f4a33c1a7c64459776b94
the-bears-field <thebearsfield+github@gmail.com>
2024-06-28 18:50:05 +0900

    build(backend): posts、commentsテーブルにuuidカラムを追加
    参考:
    https://railsguides.jp/v6.0/active_record_postgresql.html#uuid
    https://www.postgresql.jp/document/14/html/functions-uuid.html

--- backend/db/migrate/20240418053833_create_posts.rb
+++ backend/db/migrate/20240418053833_create_posts.rb
@@ -1,10 +1,13 @@
 class CreatePosts < ActiveRecord::Migration[7.1]
   def change
     create_table :posts do |t|
+      t.uuid :uuid, null: false, default: 'gen_random_uuid()'
       t.string :title, null: false
       t.text :description, null: false
 
       t.timestamps
+
+      t.index :uuid, unique: true
     end
   end
 end
