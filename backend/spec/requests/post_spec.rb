369b00cc82c992e6df4a7876b651ef9cc744152c
the-bears-field <thebearsfield+github@gmail.com>
2024-07-19 17:02:53 +0900

    test(backend): RSpecでPostsリクエストのテストを新規作成
    以下のリクエストスペックをRSpecで新規作成:
    - GET /v1/users/:user_id/postsエンドポイントのテスト
      - 投稿データが空でないことを確認
      - 投稿データの数が正しいことを確認
      - ステータスコード200が返されることを確認
    
    参考:
    Everyday Rails - RSpecによるRailsテスト入門 p.124-127
    https://rubydoc.info/gems/rspec-core/RSpec/Core/Hooks%3Abefore
    https://docs.ruby-lang.org/ja/latest/method/JSON/m/parse.html
    https://railsguides.jp/action_controller_overview.html#response%E3%82%AA%E3%83%96%E3%82%B8%E3%82%A7%E3%82%AF%E3%83%88
    https://docs.ruby-lang.org/ja/latest/class/Array.html#I_LENGTH

---
+++ backend/spec/requests/post_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "Posts", type: :request do
+  let(:user) { FactoryBot.create(:user, :with_posts) }
+
+  describe "GET /v1/users/:user_id/posts エンドポイントのテスト" do
+    before { get "/v1/users/#{user.user_id}/posts" }
+
+    it '投稿データが空でないこと、投稿データの数が正しいことを確認' do
+      expect(JSON.parse(response.body)).not_to be_empty
+      expect(JSON.parse(response.body).size).to eq(3)
+    end
+
+    it 'ステータスコード200が返されることを確認' do
+      expect(response).to have_http_status(200)
+    end
+  end
+end
