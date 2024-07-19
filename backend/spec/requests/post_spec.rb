adac6b65da000f754cb389bf93a0e0d3bf7dbbce
the-bears-field <thebearsfield+github@gmail.com>
2024-07-19 18:53:37 +0900

    test(backend): RSpecのリクエストテストを追加
    - GET /v1/users/:user_id/posts/:uuidエンドポイントのテストを新規追加
      - 特定の投稿データが空でないこと、正しいIDが返されることを確認
      - ステータスコード200が返されることを確認

--- backend/spec/requests/post_spec.rb
+++ backend/spec/requests/post_spec.rb
@@ -15,4 +15,17 @@ RSpec.describe "Posts", type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe "GET /v1/users/:user_id/posts/:uuid エンドポイントのテスト" do
+    before { get "/v1/users/#{user.user_id}/posts/#{user.posts[0].uuid}" }
+
+    it '投稿データが空でないこと、投稿データの数が正しいことを確認' do
+      expect(JSON.parse(response.body)).not_to be_empty
+      expect(JSON.parse(response.body)['id']).to eq(user.posts[0].id)
+    end
+
+    it 'ステータスコード200が返されることを確認' do
+      expect(response).to have_http_status(200)
+    end
+  end
 end
