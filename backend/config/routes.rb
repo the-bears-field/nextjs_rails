ac01da3f6afd7286be044104fe41fc77ba23e3a9
the-bears-field <thebearsfield+github@gmail.com>
2024-07-19 15:47:34 +0900

    feat(backend): ルーティングをリソースベースに変更し、名前空間を追加
    ルーティングをリソースベースに変更し、以下のとおり変更:
    - v1名前空間を追加
    - usersとpostsのルートをリソースベースに変更し、パラメータとしてuser_idとuuidを使用
    
    参考:
    https://railsguides.jp/routing.html#%E3%83%AA%E3%82%BD%E3%83%BC%E3%82%B9%E3%83%99%E3%83%BC%E3%82%B9%E3%81%AE%E3%83%AB%E3%83%BC%E3%83%86%E3%82%A3%E3%83%B3%E3%82%B0-rails%E3%81%AE%E3%83%87%E3%83%95%E3%82%A9%E3%83%AB%E3%83%88
    https://railsguides.jp/routing.html#%E3%82%B3%E3%83%B3%E3%83%88%E3%83%AD%E3%83%BC%E3%83%A9%E3%81%AE%E5%90%8D%E5%89%8D%E7%A9%BA%E9%96%93%E3%81%A8%E3%83%AB%E3%83%BC%E3%83%86%E3%82%A3%E3%83%B3%E3%82%B0

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  post 'users/create'
-  get 'users/edit'
-  post 'users/update'
-  get 'users/destory'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :v1 do
+    resources :users, param: :user_id do
+      resources :posts, param: :uuid
+    end
+  end
 end
