104cef5afe17de7e5035857ef466bbe10af28852
the-bears-field <thebearsfield+github@gmail.com>
2024-07-19 15:41:36 +0900

    feat(backend): PostsコントローラをV1モジュールに移動し機能を追加
    PostsコントローラをV1モジュールに移動し、以下のとおり変更:
    - ユーザーに関連する投稿を取得するためのset_userコールバックを追加
    - 投稿を取得するためのset_postコールバックを追加
    - indexアクションでユーザーの投稿を取得し、JSON形式で返す機能を追加
    - showアクションで特定の投稿をJSON形式で返す機能を追加
    
    参考:
    https://railsguides.jp/action_controller_overview.html#%E3%83%95%E3%82%A3%E3%83%AB%E3%82%BF
    https://railsdoc.com/page/before_action
    https://railsguides.jp/active_record_querying.html#find-by
    https://railsguides.jp/active_record_querying.html#%E4%B8%A6%E3%81%B3%E9%A0%86
    https://railsguides.jp/layouts_and_rendering.html#render%E3%83%A1%E3%82%BD%E3%83%83%E3%83%89%E3%82%92%E4%BD%BF%E3%81%86
    https://railsguides.jp/routing.html#%E3%82%B3%E3%83%B3%E3%83%88%E3%83%AD%E3%83%BC%E3%83%A9%E3%81%AE%E5%90%8D%E5%89%8D%E7%A9%BA%E9%96%93%E3%81%A8%E3%83%AB%E3%83%BC%E3%83%86%E3%82%A3%E3%83%B3%E3%82%B0

---
+++ backend/app/controllers/v1/posts_controller.rb
@@ -0,0 +1,41 @@
+module V1
+  class PostsController < ApplicationController
+    before_action :set_user, only: [:index, :show, :create, :update, :destroy]
+    before_action :set_post, only: [:show, :update, :destroy]
+
+    def index
+      @posts = @user.posts.order(created_at: :desc)
+
+      render json: @posts
+    end
+
+    def show
+      render json: @post
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+
+    def set_user
+      @user = User.find_by!(user_id: params[:user_user_id])
+    end
+
+    def set_post
+      @post = @user.posts.find_by!(uuid: params[:uuid])
+    end
+  end
+end
