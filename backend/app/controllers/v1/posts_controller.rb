2d663cead3313128aee0410a36837920d47f1f52
the-bears-field <thebearsfield+github@gmail.com>
2024-07-23 14:38:09 +0900

    refactor(backend): Postsコントローラの構造を変更
    Postsコントローラをモジュール構文からクラス構文に変更
    
    参考:
    https://railsguides.jp/routing.html#%E3%82%B3%E3%83%B3%E3%83%88%E3%83%AD%E3%83%BC%E3%83%A9%E3%81%AE%E5%90%8D%E5%89%8D%E7%A9%BA%E9%96%93%E3%81%A8%E3%83%AB%E3%83%BC%E3%83%86%E3%82%A3%E3%83%B3%E3%82%B0

--- backend/app/controllers/v1/posts_controller.rb
+++ backend/app/controllers/v1/posts_controller.rb
@@ -1,41 +1,39 @@
-module V1
-  class PostsController < ApplicationController
-    before_action :set_user, only: [:index, :show, :create, :update, :destroy]
-    before_action :set_post, only: [:show, :update, :destroy]
+class V1::PostsController < ApplicationController
+  before_action :set_user, only: [:index, :show, :create, :update, :destroy]
+  before_action :set_post, only: [:show, :update, :destroy]
 
-    def index
-      @posts = @user.posts.order(created_at: :desc)
+  def index
+    @posts = @user.posts.order(created_at: :desc)
 
-      render json: @posts
-    end
+    render json: @posts
+  end
 
-    def show
-      render json: @post
-    end
+  def show
+    render json: @post
+  end
 
-    def new
-    end
+  def new
+  end
 
-    def create
-    end
+  def create
+  end
 
-    def edit
-    end
+  def edit
+  end
 
-    def update
-    end
+  def update
+  end
 
-    def destroy
-    end
+  def destroy
+  end
 
-    private
+  private
 
-    def set_user
-      @user = User.find_by!(user_id: params[:user_user_id])
-    end
+  def set_user
+    @user = User.find_by!(user_id: params[:user_user_id])
+  end
 
-    def set_post
-      @post = @user.posts.find_by!(uuid: params[:uuid])
-    end
+  def set_post
+    @post = @user.posts.find_by!(uuid: params[:uuid])
   end
 end
