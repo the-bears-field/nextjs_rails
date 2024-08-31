6b3692b117edacd1a4ffc1ca04e8ce6de6a0faf2
the-bears-field <thebearsfield+github@gmail.com>
2024-08-31 15:08:08 +0900

    feat(backend): usersテーブルにuuidカラムを追加

--- backend/db/migrate/20240418051431_create_users.rb
+++ backend/db/migrate/20240418051431_create_users.rb
@@ -1,6 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
+      t.uuid :uuid, null: false, default: 'gen_random_uuid()'
       t.string :user_id, null: false
       t.string :name, null: false
       t.text :biography, null: false
@@ -9,6 +10,7 @@ class CreateUsers < ActiveRecord::Migration[7.1]
 
       t.timestamps
 
+      t.index :uuid, unique: true
       t.index :user_id, unique: true
       t.index :email, unique: true
       t.index :normalized_email, unique: true
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_26_053258) do
+ActiveRecord::Schema[7.2].define(version: 2024_07_26_053258) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -83,6 +83,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_26_053258) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.uuid "uuid", default: -> { "gen_random_uuid()" }, null: false
     t.string "user_id", null: false
     t.string "name", null: false
     t.text "biography", null: false
@@ -98,6 +99,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_26_053258) do
     t.index ["normalized_email"], name: "index_users_on_normalized_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["user_id"], name: "index_users_on_user_id", unique: true
+    t.index ["uuid"], name: "index_users_on_uuid", unique: true
   end
 
   add_foreign_key "post_comments", "comments"
