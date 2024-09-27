b691fafe8342f5d5edde48b5e5e132137a0a5ee3
the-bears-field <thebearsfield+github@gmail.com>
2024-09-27 14:47:34 +0900

    feat(backend): ユーザー登録内容の更新機能を実装
    以下の変更を実施:
    - `RegistrationsController`にユーザー情報の更新処理を追加
    - `update_without_password`メソッドを使用し、情報更新時のパスワードを不要化
    - サインアップおよびアカウント更新時に許可するパラメータに`user_id`, `name`, `biography`を追加
    - RSpecでユーザー情報更新に関するリクエストスペックを追加
      - ステータスコード204およびCORSヘッダーの確認
      - ユーザー情報が正しく更新されていることを確認
    
    参考:
    https://github.com/heartcombo/devise
    https://github.com/heartcombo/devise/blob/main/app/controllers/devise/registrations_controller.rb#L93

--- backend/app/controllers/v1/users/registrations_controller.rb
+++ backend/app/controllers/v1/users/registrations_controller.rb
@@ -2,7 +2,7 @@
 
 class V1::Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   # def new
@@ -20,9 +20,9 @@ class V1::Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # PUT /resource
-  # def update
-  #   super
-  # end
+  def update
+    super
+  end
 
   # DELETE /resource
   # def destroy
@@ -42,13 +42,18 @@ class V1::Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_id, :name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_id, :name, :biography])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:user_id, :name, :biography])
+  end
+
+  # デフォルトの処理を上書きし、パスワードを不要化
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
--- backend/spec/requests/v1/users/registrations_spec.rb
+++ backend/spec/requests/v1/users/registrations_spec.rb
@@ -1,26 +1,61 @@
 require 'rails_helper'
 
 RSpec.describe "V1::Users::Registrations", type: :request do
+  let(:user) { FactoryBot.create(:user) }
+  let(:auth_headers) { sign_in(user) }
+  let(:origin) { 'http://localhost' }
+
   describe "POST /v1/users エンドポイントのテスト" do
     before do
       @params = { user: FactoryBot.attributes_for(:user) }
-      post "/v1/users/", params: @params, as: :json
+      post "/v1/users/",
+        headers: { origin: origin },
+        params: @params,
+        as: :json
     end
 
     it "ステータスコード201が返されることを確認" do
       expect(response).to have_http_status(201)
     end
 
+    it 'Access-Control-Allow-Credentials が`true`であることを確認' do
+      expect(response.headers['Access-Control-Allow-Credentials']).to be_truthy
+    end
+
     it "ユーザー登録が正常に完了していることを確認" do
       expect(
-        User.where(
-          user_id: @params[:user][:user_id]
-        ).where(
-          name: @params[:user][:name]
-        ).where(
+        User.find_by(
+          user_id: @params[:user][:user_id],
+          name: @params[:user][:name],
           email: @params[:user][:email]
         ).present?
       ).to be_truthy
+    end
+  end
+
+  describe "PUT /v1/users エンドポイントのテスト" do
+    before do
+      @params = { user: { user_id: 'test', name: 'test', biography: 'test' } }
+      put '/v1/users',
+        headers: auth_headers.merge({ origin: origin }),
+        params: @params,
+        as: :json
+    end
+
+    it "ステータスコード204が返されることを確認" do
+      expect(response).to have_http_status(204)
+    end
+
+    it 'Access-Control-Allow-Credentials が`true`であることを確認' do
+      expect(response.headers['Access-Control-Allow-Credentials']).to be_truthy
+    end
+
+    it "ユーザー情報の変更が成功しているかを確認" do
+      changed_user = User.find_by(uuid: user.uuid)
+
+      expect(changed_user.user_id).to_not eq(user.user_id)
+      expect(changed_user.name).to_not eq(user.name)
+      expect(changed_user.biography).to_not eq(user.biography)
     end
   end
 end
