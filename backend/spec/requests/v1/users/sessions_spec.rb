17c9dc4f7a12ebbbb67634cb2e7ae1324fa28017
the-bears-field <thebearsfield+github@gmail.com>
2024-10-11 17:32:10 +0900

    refactor(backend): ログイン、ログアウトに関わるリクエストスペックをリファクタリングし、失敗するパターンのテストを追加

--- backend/spec/requests/v1/users/sessions_spec.rb
+++ backend/spec/requests/v1/users/sessions_spec.rb
@@ -14,43 +14,116 @@ RSpec.describe "V1::Users::Sessions", type: :request do
   }
 
   describe "POST /v1/users/sign_in エンドポイントのテスト" do
-    before do
-      post '/v1/users/sign_in',
-        params: params,
-        headers: { origin: origin },
-        as: :json
+    context "ログインが成功する" do
+      before do
+        post '/v1/users/sign_in',
+          params: params,
+          headers: { origin: origin },
+          as: :json
+      end
+
+      it "ステータスコード201が返されることを確認" do
+        expect(response).to have_http_status(201)
+      end
+
+      it 'Access-Control-Allow-Credentials が`true`であることを確認' do
+        expect(response.headers['Access-Control-Allow-Credentials']).to be_truthy
+      end
+
+      it '認証ヘッダーを返すか確認' do
+        expect(response.headers['Authorization']).to be_truthy
+      end
     end
 
-    it "ステータスコード201が返されることを確認" do
-      expect(response).to have_http_status(201)
-    end
+    context "ログインが失敗する" do
+      context "パラメーターが不正である場合" do
+        before do
+          post '/v1/users/sign_in',
+            headers: { origin: origin },
+            as: :json
+        end
 
-    it 'Access-Control-Allow-Credentials が`true`であることを確認' do
-      expect(response.headers['Access-Control-Allow-Credentials']).to be_truthy
-    end
+        it "ステータスコード401が返されることを確認" do
+          expect(response).to have_http_status(401)
+        end
 
-    it '認証ヘッダーを返すか確認' do
-      expect(response.headers['Authorization']).to be_truthy
+        it '認証ヘッダーを返さないことを確認' do
+          expect(response.headers['Authorization']).to be_falsey
+        end
+      end
+
+      context "オリジンが異なっている場合" do
+        before do
+          post '/v1/users/sign_in',
+            params: params,
+            headers: { origin: 'http://www.example.com' },
+            as: :json
+        end
+
+        it "ステータスコード403が返されることを確認" do
+          expect(response).to have_http_status(403)
+        end
+
+        it '認証ヘッダーを返さないことを確認' do
+          expect(response.headers['Authorization']).to be_falsey
+        end
+      end
     end
   end
 
   describe "DELETE /v1/users/sign_out エンドポイントのテスト" do
-    before do
-      delete '/v1/users/sign_out',
-        headers: auth_headers.merge({ origin: origin }),
-        as: :json
+    context "ログアウトが成功する" do
+      before do
+        delete '/v1/users/sign_out',
+          headers: auth_headers.merge({ origin: origin }),
+          as: :json
+      end
+
+      it "ステータスコード204が返されることを確認" do
+        expect(response).to have_http_status(204)
+      end
+
+      it 'Access-Control-Allow-Credentials が`true`であることを確認' do
+        expect(response.headers['Access-Control-Allow-Credentials']).to be_truthy
+      end
+
+      it '認証ヘッダーを返さないことを確認' do
+        expect(response.headers['Authorization']).to be_falsey
+      end
     end
 
-    it "ステータスコード204が返されることを確認" do
-      expect(response).to have_http_status(204)
-    end
+    context "ログアウトが失敗する" do
+      context "オリジンが異なっている場合" do
+        before do
+          delete '/v1/users/sign_out',
+            headers: auth_headers.merge({ origin: 'http://www.example.com' }),
+            as: :json
+        end
 
-    it 'Access-Control-Allow-Credentials が`true`であることを確認' do
-      expect(response.headers['Access-Control-Allow-Credentials']).to be_truthy
-    end
+        it "ステータスコード403が返されることを確認" do
+          expect(response).to have_http_status(403)
+        end
 
-    it '認証ヘッダーを返さないことを確認' do
-      expect(response.headers['Authorization']).to be_falsey
+        it '認証ヘッダーを返さないことを確認' do
+          expect(response.headers['Authorization']).to be_falsey
+        end
+      end
+
+      context "すでにログアウトしていた場合" do
+        before do
+          delete '/v1/users/sign_out',
+            headers: { origin: origin },
+            as: :json
+        end
+
+        it "ステータスコード204が返されることを確認" do
+          expect(response).to have_http_status(204)
+        end
+
+        it '認証ヘッダーを返さないことを確認' do
+          expect(response.headers['Authorization']).to be_falsey
+        end
+      end
     end
   end
 end
