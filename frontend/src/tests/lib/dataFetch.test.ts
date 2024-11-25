8818466cf897c8b2fb28294c0238beb9daf387e8
the-bears-field <thebearsfield+github@gmail.com>
2024-11-25 14:56:34 +0900

    test(frontend): データ取得に関わる処理でのモックデータの生成方法を変更

--- frontend/src/tests/lib/dataFetch.test.ts
+++ frontend/src/tests/lib/dataFetch.test.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { fetchPosts } from "@/lib/dataFetch";
 import { postSchema } from "@/lib/schemas";
-import { v4 as uuidv4 } from "uuid";
+import { generateMockPosts } from "@/lib/mocks/generateMockData";
 
 // axiosをモック化
 jest.mock("axios");
@@ -11,26 +11,7 @@ const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe("fetchPosts", () => {
   it("正しいデータを返す", async () => {
-    const mockData = [
-      {
-        uuid: uuidv4(),
-        title: "post title",
-        description: "Post Description",
-        created_at: "2024-01-01T00:00:00Z",
-        updated_at: "2024-01-01T00:00:00Z",
-        tags: [{ name: "example" }],
-        comments: [
-          {
-            uuid: uuidv4(),
-            description: "Commnet Description",
-            created_at: "2024-01-01T00:00:00Z",
-            updated_at: "2024-01-01T00:00:00Z",
-            users: [{ user_id: "user_2", name: "user_2" }],
-          },
-        ],
-        users: [{ user_id: "user_1", name: "user_1" }],
-      },
-    ];
+    const mockData = generateMockPosts(3);
 
     // 非同期で成功したPromiseを返すモック関数
     mockedAxios.get.mockResolvedValue({ data: mockData });
