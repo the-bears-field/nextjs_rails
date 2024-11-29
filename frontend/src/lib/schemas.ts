16fe104abdf4e5ea0f9f0ddde7a4f84608a622a4
the-bears-field <thebearsfield+github@gmail.com>
2024-11-29 09:22:52 +0900

    feat(frontend): ユーザーIDを検証する処理を追加
    - URL検証用のスキーマ宣言処理を上部に移動
    - ユーザーID検証用のスキーマ宣言を追加
      - 正規表現で4字以上、15字以下、大文字小文字の英数字とアンダーバーのみを許容するよう記述
    
    参考:
    https://zod.dev/?id=strings
    https://developer.mozilla.org/ja/docs/Web/JavaScript/Guide/Regular_expressions

--- frontend/src/lib/schemas.ts
+++ frontend/src/lib/schemas.ts
@@ -1,11 +1,18 @@
 import { z } from "zod";
 
+export const urlSchema = z.string().url();
+export const userIdSchema = z
+  .string()
+  .min(4)
+  .max(15)
+  .regex(/^[a-zA-Z0-9_]{4,15}$/);
+
 export const tagSchema = z.object({
   name: z.string(),
 });
 
 export const userSchema = z.object({
-  user_id: z.string().min(4).max(15),
+  user_id: userIdSchema,
   name: z.string(),
 });
 
@@ -27,5 +34,3 @@ export const postSchema = z.object({
   comments: z.array(commentSchema),
   users: z.array(userSchema),
 });
-
-export const urlSchema = z.string().url();
