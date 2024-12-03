e4be31c4cc0a8827b0a6834f0624f16a0b86cf7e
the-bears-field <thebearsfield+github@gmail.com>
2024-12-03 11:19:48 +0900

    feat(frontend): 複数の投稿データ取得の処理を変更
    - Zodで検証済のデータを取得する関数をインポート
    - 検証済みのユーザーID、URLを取得する処理を関数化し、複数の投稿データを取得する関数内で使用

--- frontend/src/lib/dataFetch.ts
+++ frontend/src/lib/dataFetch.ts
@@ -1,17 +1,19 @@
 import axios, { AxiosResponse } from "axios";
-import { postSchema, urlSchema } from "./schemas";
+import { postSchema, urlSchema, userIdSchema } from "./schemas";
 import { z } from "zod";
+import { generateParsedData } from "./generateParsedData";
 
 type Post = z.infer<typeof postSchema>;
 
 const origin: string = "http://web";
 
 /** APIサーバーから複数の投稿データを取得 */
-export async function fetchPosts(user_id: string): Promise<Post[]> {
-  const url: string = generateUrl(`/v1/users/${user_id}/posts`);
+export async function fetchPosts(userId: string): Promise<Post[]> {
+  const parsedUserId: string = generateUserId(userId);
+  const parsedUrl: string = generateUrl(`/v1/users/${parsedUserId}/posts`);
 
   try {
-    const response: AxiosResponse = await axios.get(url, {
+    const response: AxiosResponse = await axios.get(parsedUrl, {
       insecureHTTPParser: true,
     });
 
@@ -37,11 +39,13 @@ export async function fetchPosts(user_id: string): Promise<Post[]> {
   }
 }
 
-/** URLを検証して生成する関数 */
+/** URLを検証、生成する関数 */
 function generateUrl(path: string): string {
-  const parsedUrl = urlSchema.safeParse(`${origin}${path}`);
+  const url: string = `${origin}${path}`;
+  return generateParsedData({ schema: urlSchema, data: url });
+}
 
-  if (!parsedUrl.success) throw new Error(parsedUrl.error.message);
-
-  return parsedUrl.data;
+/** ユーザーIdを検証、生成する関数 */
+function generateUserId(userId: string): string {
+  return generateParsedData({ schema: userIdSchema, data: userId });
 }
