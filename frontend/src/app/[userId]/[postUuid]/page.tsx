17df725b6c39634f24f1cc3adb2b111a583d221d
the-bears-field <thebearsfield+github@gmail.com>
2024-12-03 14:34:25 +0900

    feat(frontend): 単一の投稿を表示するページを作成
    - ユーザーIDと投稿のUUIDの値を用いて動的ルーティングに対応

---
+++ frontend/src/app/[userId]/[postUuid]/page.tsx
@@ -0,0 +1,40 @@
+import { fetchPost, fetchPosts } from "@/lib/dataFetch";
+import { postSchema } from "@/lib/schemas";
+import { z } from "zod";
+
+type Post = z.infer<typeof postSchema>;
+
+export default async function Page({
+  params: { userId, postUuid },
+}: {
+  params: { userId: string; postUuid: string };
+}) {
+  const postData: Post = await fetchPost({
+    userId: userId,
+    postUuid: postUuid,
+  });
+
+  return (
+    <main>
+      <div className="min-w-full">
+        <article className="m-16">
+          <h2 className="text-xl">{postData.title}</h2>
+          <p className="mt-4">{postData.description}</p>
+          <div className="flex flex-row mt-4">
+            {postData.tags.map((tag, key) => (
+              <span key={key} className="mr-6">
+                {`#${tag.name}`}
+              </span>
+            ))}
+          </div>
+          {postData.comments.map((comment, key) => (
+            <div key={key} className="m-4 ">
+              <p className="text-sm">{comment.users[0].name}</p>
+              <p>{comment.description}</p>
+            </div>
+          ))}
+        </article>
+      </div>
+    </main>
+  );
+}
