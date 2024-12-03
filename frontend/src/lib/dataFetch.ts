a62ba064963b351a72edc819aa3abf9b325694ae
the-bears-field <thebearsfield+github@gmail.com>
2024-12-03 14:28:24 +0900

    feat(frontend): 単一の投稿データ取得の処理を追加

--- frontend/src/lib/dataFetch.ts
+++ frontend/src/lib/dataFetch.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosResponse } from "axios";
-import { postSchema, urlSchema, userIdSchema } from "./schemas";
+import { postSchema, urlSchema, userIdSchema, uuidSchema } from "./schemas";
 import { z } from "zod";
 import { generateParsedData } from "./generateParsedData";
 
@@ -39,6 +39,42 @@ export async function fetchPosts(userId: string): Promise<Post[]> {
   }
 }
 
+/** APIサーバーから単一の投稿データを取得 */
+export async function fetchPost(params: { userId: string; postUuid: string }) {
+  const { userId, postUuid } = params;
+  const parsedUserId: string = generateUserId(userId);
+  const parsedPostUuid: string = generateUuid(postUuid);
+  const parsedUrl: string = generateUrl(
+    `/v1/users/${parsedUserId}/posts/${parsedPostUuid}`
+  );
+
+  try {
+    const response: AxiosResponse = await axios.get(parsedUrl, {
+      insecureHTTPParser: true,
+    });
+
+    const parsedData = postSchema.safeParse(response.data);
+
+    if (!parsedData.success) {
+      parsedData.error.issues.forEach((issue) => {
+        console.error(
+          `バリデーションエラー ${issue.path.join(".")}: ${issue.message}`
+        );
+      });
+      throw new Error("無効なレスポンスデータ形式です");
+    }
+
+    return parsedData.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.error("API呼び出しに失敗:", error.message);
+      throw new Error("APIからの投稿の取得に失敗しました");
+    }
+    console.error("予期しないエラー:", error);
+    throw new Error("予期しないエラーが発生しました");
+  }
+}
+
 /** URLを検証、生成する関数 */
 function generateUrl(path: string): string {
   const url: string = `${origin}${path}`;
@@ -48,4 +84,9 @@ function generateUrl(path: string): string {
 /** ユーザーIdを検証、生成する関数 */
 function generateUserId(userId: string): string {
   return generateParsedData({ schema: userIdSchema, data: userId });
+}
+
+/** UUIDを検証、生成する関数 */
+function generateUuid(uuid: string): string {
+  return generateParsedData({ schema: uuidSchema, data: uuid });
 }
