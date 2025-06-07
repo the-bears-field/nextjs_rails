1a2320da965f7a058e111a822f02449d8d2125b3
the-bears-field <thebearsfield+github@gmail.com>
2025-06-07 15:38:26 +0900

    feat(frontend): ユーザーモック生成に関わる変更
    - `generateMockUser`関数に export 宣言を使用。
    - 戻り値のオブジェクトに`email`、`password`キーと値を追加。
      - `email`キーの値はfakerを用いて定義。
      - `password`キーの値は、APIサーバーで定めている値を必ず返すよう`generatePassword`関数を作成し、その戻り値を定義。
    
    参考:
    https://fakerjs.dev/api/internet.html#exampleemail
    https://nodejs.org/api/crypto.html#cryptorandomintmin-max-callback
    http://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Array/push
    https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/String/charAt
    https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/String/length
    https://ja.wikipedia.org/wiki/%E3%83%95%E3%82%A3%E3%83%83%E3%82%B7%E3%83%A3%E3%83%BC%E2%80%93%E3%82%A4%E3%82%A7%E3%83%BC%E3%83%84%E3%81%AE%E3%82%B7%E3%83%A3%E3%83%83%E3%83%95%E3%83%AB
    https://www.pandanoir.info/entry/2013/03/04/193704
    https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Operators/Destructuring#%E5%A4%89%E6%95%B0%E3%81%AE%E5%85%A5%E3%82%8C%E6%9B%BF%E3%81%88
    https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Array/join

--- frontend/src/lib/mocks/generateMockData.ts
+++ frontend/src/lib/mocks/generateMockData.ts
@@ -19,12 +19,14 @@ function generateMockTag(): Tag {
 }
 
 // ユーザーのモックを生成する関数
-function generateMockUser(): User {
+export function generateMockUser(): User {
   return {
     user_id: faker.word.noun({
       length: { min: 4, max: 15 },
     }),
     name: faker.person.fullName(),
+    email: faker.internet.exampleEmail(),
+    password: generatePassword(),
   };
 }
 
@@ -51,4 +53,44 @@ function generateMockPost(): Post {
     comments: Array.from({ length: faker.number.int(5) }, generateMockComment),
     users: [generateMockUser()],
   };
+}
+
+/**
+ * 8〜100文字の範囲で
+ * - 小文字1文字以上
+ * - 大文字1文字以上
+ * - 数字1文字以上
+ * - 記号1文字以上
+ * を必ず含む文字列を生成する関数
+ */
+function generatePassword(): string {
+  const { randomInt } = require("node:crypto");
+
+  // すべての候補文字を明示的に定義(英小文字、英大文字、数字、記号)
+  const LOWERCASE: string = "abcdefghijklmnopqrstuvwxyz";
+  const UPPERCASE: string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const DIGISTS: string = "0123456789";
+  const SYMBOLS: string = "!@#$%^&*()_+-=[]{};':\"\\|,.<>/?";
+  const ALL_CHARS: string = LOWERCASE + UPPERCASE + DIGISTS + SYMBOLS;
+  const length: number = randomInt(8, 100);
+
+  // 結果を格納する配列を定義し、必須文字を確実に追加
+  const result: string[] = [];
+  result.push(LOWERCASE.charAt(randomInt(0, LOWERCASE.length)));
+  result.push(UPPERCASE.charAt(randomInt(0, UPPERCASE.length)));
+  result.push(DIGISTS.charAt(randomInt(0, DIGISTS.length)));
+  result.push(SYMBOLS.charAt(randomInt(0, SYMBOLS.length)));
+
+  // 残りの文字を`ALL_CHARS`からランダムに抽出
+  for (let i = result.length; i <= length; i++) {
+    result.push(ALL_CHARS.charAt(randomInt(0, ALL_CHARS.length)));
+  }
+
+  // Fisher-Yatesアルゴリズムを用いて、順序をランダムに並び替え
+  for (let i = result.length - 1; i > 0; i--) {
+    const j = randomInt(0, i + 1);
+    [result[i], result[j]] = [result[j], result[i]];
+  }
+
+  return result.join("");
 }
