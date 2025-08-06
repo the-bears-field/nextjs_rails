fe1e3526856462bdafdbd56c401400e8195c97c4
Satoshi <thebearsfield+github@gmail.com>
2025-08-06 13:49:50 +0900

    fix(backend): Apple Siliconでバックエンドコンテナが起動しなくなる問題に対応
    `backend/tmp/sockets/puma.sock`を削除し再起動すると症状が改善したため、コンテナ起動時に実行されるスクリプトに、ソケット通信に必要なファイルを削除する処理を追加。
    
    /usr/local/bundle/gems/puma-6.6.0/lib/puma/binder.rb:194:in `realdirpath': Operation not supported @ realpath_rec - /app/tmp/sockets/puma.sock (Errno::ENOTSUP)
    
    参考:
    https://github.com/puma/puma/blob/master/lib/puma/binder.rb#L194
    https://docs.ruby-lang.org/ja/latest/class/File.html#S_REALDIRPATH
    https://docs.ruby-lang.org/ja/latest/class/Errno=3a=3aENOTSUP.html
    https://docs.ruby-lang.org/en/master/Errno.html#ENOTSUP

--- docker/backend/entrypoint.sh
+++ docker/backend/entrypoint.sh
@@ -4,5 +4,8 @@ set -e
 # Rails に対応したファイル server.pid が存在しているかもしれないので削除する。
 rm -f /app/tmp/pids/server.pid
 
+# コンテナ起動時に puma.sock を削除
+rm -f /app/tmp/sockets/puma.sock
+
 # コンテナーのプロセスを実行する。（Dockerfile 内の CMD に設定されているもの。）
 exec "$@"
