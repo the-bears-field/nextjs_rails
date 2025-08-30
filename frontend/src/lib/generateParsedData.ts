5bb87e8df8d9e17fc2f9a9f262ea174f104e1115
Satoshi <thebearsfield+github@gmail.com>
2025-08-30 13:48:25 +0900

    refactor(frontend): Zodを用いた検証をする関数の変更
    - `export`宣言が不要な箇所を削除。
    - `generateParsedData`関数の戻り値の型指定方法を型注釈に変更。
    - その他、軽微な修正。
    
    参考:
    https://www.typescriptlang.org/docs/handbook/2/everyday-types.html#return-type-annotations
    https://www.typescriptlang.org/docs/handbook/2/everyday-types.html#type-assertions
    https://typescriptbook.jp/reference/values-types-variables/type-assertion-as
    https://typescriptbook.jp/reference/functions/function-type-declaration

--- frontend/src/lib/generateParsedData.ts
+++ frontend/src/lib/generateParsedData.ts
@@ -2,24 +2,24 @@ import { z } from "zod/v4";
 import { urlSchema, userIdSchema, uuidSchema } from "./schemas";
 
 /** Zodで検証してデータを返す関数 */
-export function generateParsedData<T extends z.ZodType>(params: {
+function generateParsedData<T extends z.ZodType>(params: {
   schema: T;
   data: unknown;
-}) {
+}): z.infer<T> {
   const { schema, data } = params;
   const parsedData = schema.safeParse(data);
   if (!parsedData.success) throw new Error(parsedData.error.message);
-  return parsedData.data as z.infer<T>;
+  return parsedData.data;
 }
 
 /** URLを検証、生成する関数 */
-export function generateUrl(params: { origin: string; path: string }): string {
+function generateUrl(params: { origin: string; path: string }): string {
   const { origin, path } = params;
   const url: string = `${origin}${path}`;
   return generateParsedData({ schema: urlSchema, data: url });
 }
 
-//** Dockerのコンテナ用のURLを検証、生成する関数 */
+/** Dockerのコンテナ用のURLを検証、生成する関数 */
 export function generateContainerUrl(path: string): string {
   const origin: string = "http://web";
   return generateUrl({ origin: origin, path: path });
