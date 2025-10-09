0b5d312c3166e8ff7fecd00dfbeec8c693afed8d
Satoshi <thebearsfield+github@gmail.com>
2025-10-10 06:21:23 +0900

    feat(frontend): Result型を追加
    参考:
    JavaScript/TypeScript実力強化書 p.160
    https://speakerdeck.com/kodak4400/typescriptno-resultxing-nosu-me

--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -12,3 +12,15 @@ export type PostUser = z.infer<typeof postUserSchema>;
 export type Post = z.infer<typeof postSchema>;
 export type Tag = z.infer<typeof tagSchema>;
 export type Comment = z.infer<typeof commentSchema>;
+
+type Success<T> = {
+  success: true;
+  value: T;
+};
+
+type Failure<E> = {
+  success: false;
+  errors: E;
+};
+
+export type Result<T, E = unknown> = Success<T> | Failure<E>;
