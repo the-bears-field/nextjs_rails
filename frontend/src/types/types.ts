28da8d46953e1d72d5a3e5c363804792a8be5b87
Satoshi <thebearsfield+github@gmail.com>
2025-10-24 16:58:45 +0900

    revert(frontend): 型の宣言変更の取消
    - 型の堅牢性を考慮し 型エイリアスで運用する方針に変更。
    
    参考:
    https://kosui.me/posts/2025/10/23/214710
    https://x.com/uhyo_/status/1980824323503059097
    https://www.typescriptlang.org/docs/handbook/2/everyday-types.html#differences-between-type-aliases-and-interfaces

--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -1,96 +1,26 @@
-/**
- * APIから取得する`User`の型
- * @property {string} user_id
- * @property {string} name
- * @property {string} email
- * @property {string} password
- */
-export interface User {
-  user_id: string;
-  name: string;
-  email: string;
-  password: string;
-}
+import z from "zod/v4";
+import {
+  commentSchema,
+  postSchema,
+  postUserSchema,
+  tagSchema,
+  userSchema,
+} from "../lib/schemas";
 
-/**
- * 記事表示に使用する`PostUser`の型
- * @property {string} user_id
- * @property {string} name
- */
-export interface PostUser extends Pick<User, "user_id" | "name"> {}
+export type User = z.infer<typeof userSchema>;
+export type PostUser = z.infer<typeof postUserSchema>;
+export type Post = z.infer<typeof postSchema>;
+export type Tag = z.infer<typeof tagSchema>;
+export type Comment = z.infer<typeof commentSchema>;
 
-/**
- * APIから取得する`Post`型を定義
- * @property {string} uuid
- * @property {string} title
- * @property {string} description
- * @property {string} created_at
- * @property {string} updated_at
- * @property {Tag[]} tags
- * @property {Comment[]} comments
- * @property {PostUser[]} users
- */
-export interface Post {
-  uuid: string;
-  title: string;
-  description: string;
-  created_at: string;
-  updated_at: string;
-  tags: Tag[];
-  comments: Comment[];
-  users: PostUser[];
-}
-
-/**
- * APIから取得する`Tag`型を定義
- * @property {string} name
- */
-export interface Tag {
-  name: string;
-}
-
-/**
- * APIから取得する`Comment`の型
- * @property {string} uuid
- * @property {string} title
- * @property {string} description
- * @property {string} created_at
- * @property {string} updated_at
- * @property {PostUser[]} users
- */
-export interface Comment {
-  uuid: string;
-  description: string;
-  created_at: string;
-  updated_at: string;
-  users: PostUser[];
-}
-
-/**
- * 処理が成功した時の型
- * @template T 成功時の値の型
- * @property {true} success
- * @property {T} value
- */
-interface Success<T> {
+type Success<T> = {
   success: true;
   value: T;
-}
+};
 
-/**
- * 処理が失敗した時の型
- * @template E 失敗時のエラーの型
- * @property {false} success
- * @property {E} errors
- */
-interface Failure<E> {
+type Failure<E> = {
   success: false;
   errors: E;
-}
+};
 
-/**
- * 汎用的な結果型
- * @template T 成功時の値の型
- * @template E 失敗時のエラーの型
- */
 export type Result<T, E = unknown> = Success<T> | Failure<E>;
