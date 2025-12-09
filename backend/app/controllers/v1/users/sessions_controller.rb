3277f7b80f36535f5e7885b12b0afe51dfe37e92
Satoshi <thebearsfield+github@gmail.com>
2025-12-09 14:46:12 +0900

    feat(backend): ログイン機能をAPIモードに適した内容に変更
    参考:
    https://github.com/heartcombo/devise/blob/main/app/controllers/devise/sessions_controller.rb#L18
    https://github.com/wardencommunity/warden/blob/master/lib/warden/proxy.rb#L109

--- backend/app/controllers/v1/users/sessions_controller.rb
+++ backend/app/controllers/v1/users/sessions_controller.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 class V1::Users::SessionsController < Devise::SessionsController
+  before_action :validate_origin, only: [:create, :destroy]
   before_action :configure_sign_in_params, only: [:create]
+
+  respond_to :json
 
   # GET /resource/sign_in
   # def new
@@ -9,8 +12,26 @@ class V1::Users::SessionsController < Devise::SessionsController
   # end
 
   # POST /resource/sign_in
+  #
+  # @return [JSON] サインイン結果のJSONレスポンス
+  #   成功時: { value: { user_id: String }, success: true }
+  #   失敗時: { errors: [String], success: false }
   def create
-    super
+    resource = warden.authenticate(auth_options)
+
+    if resource
+      sign_in(resource_name, resource)
+
+      render json: {
+        value: resource.as_json(only: [:user_id]),
+        success: true
+      }, status: :created
+    else
+      render json: {
+        errors: [I18n.t('devise.failure.invalid', default: 'Invalid email or password.')],
+        success: false
+      }, status: :unauthorized
+    end
   end
 
   # DELETE /resource/sign_out
