725095b2c753cc38e33c89ec3ed75b5088831b00
Satoshi <thebearsfield+github@gmail.com>
2026-05-06 19:38:21 +0900

    fix(frontend): CSSインポートに対するモジュールまたは型宣言が見つからない問題を修正
    参考:
    https://blog.shimar.me/2020/12/05/nextjs-css-cannot-find-module

--- frontend/next-env.d.ts
+++ frontend/next-env.d.ts
@@ -1,6 +1,8 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 /// <reference path="./.next/types/routes.d.ts" />
+declare module "*.css";
+declare module "*.scss";
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
