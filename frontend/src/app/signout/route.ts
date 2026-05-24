7e1b04ebea36f4d679c7459ccb58b7cee5857039
Satoshi <thebearsfield+github@gmail.com>
2026-05-24 20:43:44 +0900

    chore(frontend): サインアウトのパスを削除

--- frontend/src/app/signout/route.ts
+++
@@ -1,7 +0,0 @@
-import { redirect } from "next/navigation";
-import { signout } from "@/features/authentications/actions/signout";
-
-export async function GET() {
-  await signout();
-  redirect("/");
-}
