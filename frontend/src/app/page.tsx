46ab820096a0f3a1be7de1e653768195ebfa7a88
Satoshi <thebearsfield+github@gmail.com>
2026-06-22 21:27:26 +0900

    style(frontend): トップページのデザインを変更

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,113 +1,140 @@
-import Image from 'next/image'
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+    <section className="mx-auto max-w-max">
+      <h1 className="text-4xl font-bold">
+        <Link
+          href="https://github.com/the-bears-field/nextjs_rails"
+          className="text-blue-400 hover:underline"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          Rails API、Next.js を使用したブログアプリ
+        </Link>
+      </h1>
+      <article className="mt-16 ml-8">
+        <h2 className="text-2xl font-bold">使用技術</h2>
+        <article className="mt-8">
+          <h3 className="text-xl font-bold">フロントエンド</h3>
+          <ul className="list-disc list-inside ml-4">
+            <li>Node.js v24.15.0</li>
+            <li>react@19.1.1</li>
+            <li>typescript@6.0.3</li>
+            <li>next@15.5.2</li>
+            <li>zod@4.1.5</li>
+            <li>tailwindcss@4.1.13</li>
+            <li>jest@29.7.0</li>
+          </ul>
+        </article>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        <article className="mt-8">
+          <h3 className="text-xl font-bold">バックエンド</h3>
+          <ul className="list-disc list-inside ml-4">
+            <li>Ruby 4.0.2</li>
+            <li>Rails 8.0.5</li>
+            <li>RSpec 3.13.6</li>
+          </ul>
+        </article>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+        <article className="mt-8">
+          <h3 className="text-xl font-bold">データベース</h3>
+          <ul className="list-disc list-inside ml-4">
+            <li>PostgreSQL 17.5</li>
+          </ul>
+        </article>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+        <article className="mt-8">
+          <h3 className="text-xl font-bold">Web サーバー</h3>
+          <ul className="list-disc list-inside ml-4">
+            <li>nginx/1.28.0</li>
+          </ul>
+        </article>
+      </article>
+      <article className="mt-16 ml-8">
+        <h2 className="text-2xl font-bold">必要要件</h2>
+        <ul className="list-disc list-inside ml-4">
+          <li>Git</li>
+          <li>Docker</li>
+          <li>Docker Compose</li>
+        </ul>
+      </article>
+      <article className="mt-16 ml-8">
+        <h2 className="text-2xl font-bold">インストール</h2>
+        <section className="mt-8">
+          <p>
+            必要要件に記載している環境を整えた上で、ターミナルで下記コマンドを順番に実行してください。
           </p>
-        </a>
-      </div>
-    </main>
-  )
+          <div className="mt-4">
+            <pre className="p-4 rounded-2xl bg-gray-200 dark:bg-gray-600 select-all">
+              <code>
+                git clone https://github.com/the-bears-field/nextjs_rails.git
+              </code>
+            </pre>
+          </div>
+          <div className="mt-4">
+            <pre className="p-4 rounded-2xl bg-gray-200 dark:bg-gray-600 select-all">
+              <code>cd nextjs_rails</code>
+            </pre>
+          </div>
+          <div className="mt-4">
+            <pre className="p-4 rounded-2xl bg-gray-600 select-all">
+              <code>sh initialization.sh</code>
+            </pre>
+          </div>
+          <div className="mt-4">
+            <p>
+              以上の過程を経てブラウザで
+              <Link
+                href="http://localhost"
+                className="text-blue-400 hover:underline"
+              >
+                http://localhost
+              </Link>
+              を開くと、ポートフォリオの閲覧が可能となります。
+            </p>
+          </div>
+        </section>
+      </article>
+      <article className="mt-16 ml-8">
+        <h2 className="text-2xl font-bold">既定のユーザー情報</h2>
+        <article className="mt-4">
+          <h3 className="text-xl font-bold">
+            <Link href="/user_1" className="text-blue-400 hover:underline">
+              User1
+            </Link>
+          </h3>
+          <div className="mt-4 ml-4">
+            <h4 className="font-bold">mail</h4>
+            <pre className="p-4 rounded-2xl bg-gray-200 dark:bg-gray-600 select-all">
+              <code>user1@example.com</code>
+            </pre>
+          </div>
+          <div className="mt-4 ml-4">
+            <h4 className="font-bold">password</h4>
+            <pre className="p-4 rounded-2xl bg-gray-200 dark:bg-gray-600 select-all">
+              <code>Password@1234</code>
+            </pre>
+          </div>
+        </article>
+        <article className="mt-8">
+          <h3 className="text-xl font-bold">
+            <Link href="/user_2" className="text-blue-400 hover:underline">
+              User2
+            </Link>
+          </h3>
+          <div className="mt-4 ml-4">
+            <h4 className="font-bold">mail</h4>
+            <pre className="p-4 rounded-2xl bg-gray-200 dark:bg-gray-600 select-all">
+              <code>user2@example.com</code>
+            </pre>
+          </div>
+          <div className="mt-4 ml-4">
+            <h4 className="font-bold">password</h4>
+            <pre className="p-4 rounded-2xl bg-gray-200 dark:bg-gray-600 select-all">
+              <code>Password@1234</code>
+            </pre>
+          </div>
+        </article>
+      </article>
+    </section>
+  );
 }
